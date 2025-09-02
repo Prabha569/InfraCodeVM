c700f5aca366f9d64b33f36c5511b7de1208aa1f
Prabha569 <prabhakarnayak993@gmail.com>
2025-09-02 11:14:04 +0530

    Updated code

---
+++ modules/azurerm_storage_account/variable.tf
@@ -0,0 +1,20 @@
+variable "stg_name" {
+  description = "This is stg name"
+  type = string
+}
+variable "resource_group_name" {
+  description = "This is rg name"
+  type = string
+}
+variable "location" {
+  description = "This is stg location"
+  type = string
+}
+variable "account_tier" {
+  description = "This is account_tier name"
+  type = string
+}
+variable "account_replication_type" {
+  description = "This is account_replication  name"
+  type = string
+}
---
+++ todoapp_parent/provider.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.33.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "rg-rama"
+    storage_account_name = "krishnastg567"
+    container_name       = "tfstate"
+    key                  = "test.terraform.tfstate"
+  }
+
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "239f86cf-d0d2-4954-935f-40e39253100c"
+}
